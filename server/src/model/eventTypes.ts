76a97dad835b05d6df32769e8a38f069f81595ab
S-Maciejewski <maciejewski.torun@gmail.com>
2021-01-22 23:04:04 +0100

    Server changes

---
+++ server/src/model/eventTypes.ts
@@ -0,0 +1,11 @@
+enum EventType {
+    CONNECT = 'connect',
+    DISCONNECT = 'disconnect',
+    CREATE_ROOM = 'createRoom',
+    JOIN_ROOM = 'joinRoom',
+    START_GAME = 'startGame',
+    UPDATE_GAME_STATE = 'updateGameState',
+    UPDATE_PLAYER = 'updatePlayer'
+}
+
+export default EventType;
---
+++ server/src/model/player.ts
@@ -0,0 +1,7 @@
+export default class Player {
+    _id!: string;
+    x!: number;
+    y!: number;
+    rotation!: number;
+    speed!: number;
+}
