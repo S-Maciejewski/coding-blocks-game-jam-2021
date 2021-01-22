24a9b84b6844e7ae36024c9419158ae7510cbfe7
kyrcooler <kyrcooler@gmail.com>
2021-01-22 23:04:01 +0100

    Small changes to client UI

--- client/src/scenes/menu-scene.ts
+++ client/src/scenes/menu-scene.ts
@@ -1,9 +1,8 @@
 
-import * as io from 'socket.io-client';
+import io from 'socket.io-client';
 import Player from '../prefabs/player';
 
-const SERVER_ADDR = 'ws://localhost:3000';
-
+const SERVER_ADDR = 'http://138.197.178.99';
 export default class MenuScene extends Phaser.Scene
 {
     socket: SocketIOClient.Socket;
@@ -11,30 +10,33 @@ export default class MenuScene extends Phaser.Scene
 
     constructor ()
     {
-        super('GameScene');
+        super('MenuScene');
     }
 
     preload ()
     {
+
     }
 
     create ()
     {
-        socket = io.connect(SERVER_ADDR);
+        this.socket = io(SERVER_ADDR);
 
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
         
-        var roomCodeInputField = this.getChildByName('roomCode');
-        roomCodeInputField.styles.display = 'initial';
+        var roomCodeInputField = (<HTMLInputElement>document.getElementById('roomCode'));
+        roomCodeInputField.style.display = 'initial';
+        roomCodeInputField.style.position = 'absolute';
 
         const createRoomButton = this.add.text(screenCenterX, screenCenterY, 'Create Room');
+        createRoomButton.x -= createRoomButton.width/2;
         createRoomButton.setInteractive();
         createRoomButton.on('pointerdown', () => { this.socket.emit('createRoom'); });
 
         const joinRoomButton = this.add.text(screenCenterX, screenCenterY + 50, 'Join Room');
+        joinRoomButton.x -= joinRoomButton.width/2;
         joinRoomButton.setInteractive();
         joinRoomButton.on('pointerdown', () => { this.socket.emit('joinRoom', roomCodeInputField.value); });
-
     }
 }
